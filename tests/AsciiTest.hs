9537893815c0046a221d3a679c798d27d009dcfd
chessai <chessai1996@gmail.com>
2018-02-01 11:07:10 -0500

    add simple test suite

--- tests/AsciiTest.hs
+++ tests/AsciiTest.hs
@@ -5,11 +5,16 @@ import Test.QuickCheck
 
 import qualified Data.ByteString as B
 
+sizedByteString :: Int -> Gen B.ByteString
 sizedByteString n = do m <- choose (0, n)
                        fmap B.pack $ vectorOf m arbitrary
 
 instance Arbitrary B.ByteString where
   arbitrary = do
-  bs <- sized sizedByteString
-  n  <- choose (0,2)
-  pure (B.drop n bs) -- to give us some with non-0 offset
+    bs <- sized sizedByteString
+    n  <- choose (0,7)
+    m  <- choose (0,7) 
+    pure (B.take m $ B.drop n bs) -- to give us some with non-0 offset
+
+testIsAscii :: IO ()
+testIsAscii = quickCheck (withMaxSuccess 10000 isAscii)
